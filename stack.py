e5bfa2e076e6f61fd84b28930d00ab523b443205
mbogonjuguna <85078937+mbogonjuguna@users.noreply.github.com>
2021-05-30 22:56:05 +0300

    Add is_empty method

--- stack.py
+++ stack.py
@@ -1,6 +1,7 @@
 from collections import deque
-students = deque()    #intialize students using class deque
+students = deque()          #intialize students using class deque
 students.append('James')    #add elements to stack 
 students.append( 'John')              
-students.pop()                        #display and delete last element appended element                              
+students.pop()              #display and delete last element appended element 
+print(students.is_empty())  #Print true if stack is empty and false if not  
 print(students)    

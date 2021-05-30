afe26b05bfbe85014745d4c1e3f365304f33428f
mbogonjuguna <85078937+mbogonjuguna@users.noreply.github.com>
2021-05-30 23:15:28 +0300

    Back to first commit

--- stack.py
+++ stack.py
@@ -2,5 +2,5 @@ from collections import deque
 students = deque()    #intialize students using class deque
 students.append('James')    #add elements to stack 
 students.append( 'John')              
-students.pop()                        #display and delete last element appended element                              
+students.pop()                    #display and delete last element appended element                              
 print(students)    
